f2d2878ebe568eb9a087eeda5f164d2a1b34be06
Hieu4438 <520H0361@student.tdtu.edu.vn>
2022-11-11 08:38:57 +0700

    add-test: check_gradeC, check_gradeD

--- TestLab9/UnitTest1.cs
+++ TestLab9/UnitTest1.cs
@@ -19,10 +19,33 @@ namespace TestLab9
         public void Test_ScoreBelow8LargeEqual7_GradeB()
         {
             char res = 'B';
-            double[] cases = {7, 7.0, 7.5, 7.9 };
+            double[] cases = {7, 7.0, 7.5, 7.9};
             Test_WhenScore_GradeIs(cases, res);
         }
 
+        [TestMethod]
+        public void Test_ScoreBelow7LargeEqual5_GradeC()
+        {
+            char res = 'C';
+            double[] cases = { 6.9, 6.5, 6.0, 5,5.0,5.1};
+            Test_WhenScore_GradeIs(cases, res);
+        }
+
+        [TestMethod]
+        public void Test_ScoreBelow5LargeEqual3half_GradeD()
+        {
+            char res = 'D';
+            double[] cases = {4.9, 4.5, 3.5, 3.50, 3.6, 3.9};
+            Test_WhenScore_GradeIs(cases, res);
+        }
+
+        [TestMethod]
+        public void Test_ScoreBelow3half_GradeE()
+        {
+            char res = 'E';
+            double[] cases = {3.49, 3.4, 0, 3.2, 2.5};
+            Test_WhenScore_GradeIs(cases, res);
+        }
 
         public void Test_WhenScore_GradeIs(double[] cases, char res)
         {
