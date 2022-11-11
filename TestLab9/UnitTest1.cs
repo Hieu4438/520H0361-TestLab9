16db0a867578423427789bf899e59747beead055
Hieu4438 <520H0361@student.tdtu.edu.vn>
2022-11-11 08:26:57 +0700

    add-test: scoreLarger8_gradeA

--- TestLab9/UnitTest1.cs
+++ TestLab9/UnitTest1.cs
@@ -1,12 +1,22 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollector.InProcDataCollector;
+using StudentServiceLib;
+
 namespace TestLab9
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Score8_GradeA()
         {
-
+            char res = 'A';
+            Student a = new Student();
+            double[] cases = {8.0,8,8.5,9,9.5,10};
+            for (int i = 0; i < cases.Length; i++)
+            {
+                a.Score = cases[i];
+                Assert.AreEqual(res, a.getLetterScore());
+            }
         }
     }
 }   
