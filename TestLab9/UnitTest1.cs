ec8e1db852c54d702d075301f148b150a613439b
Hieu4438 <520H0361@student.tdtu.edu.vn>
2022-11-11 08:33:01 +0700

    add-test: scoreBelow8From7_gradeB

--- TestLab9/UnitTest1.cs
+++ TestLab9/UnitTest1.cs
@@ -10,8 +10,24 @@ namespace TestLab9
         public void Test_Score8_GradeA()
         {
             char res = 'A';
-            Student a = new Student();
+            
             double[] cases = {8.0,8,8.5,9,9.5,10};
+            Test_WhenScore_GradeIs(cases, res);
+        }
+
+        [TestMethod]
+        public void Test_ScoreBelow8LargeEqual7_GradeB()
+        {
+            char res = 'B';
+            double[] cases = {7, 7.0, 7.5, 7.9 };
+            Test_WhenScore_GradeIs(cases, res);
+        }
+
+
+        public void Test_WhenScore_GradeIs(double[] cases, char res)
+        {
+
+            Student a = new Student();
             for (int i = 0; i < cases.Length; i++)
             {
                 a.Score = cases[i];
